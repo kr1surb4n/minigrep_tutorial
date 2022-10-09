7fc742c200a244a4ff98c0791f1e3f946aedaa65
Kris Urbanski <kris@whereibend.space>
2022-10-09 16:44:27 +0200

    autosave

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     // "example-filename.txt",
     // ]
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
