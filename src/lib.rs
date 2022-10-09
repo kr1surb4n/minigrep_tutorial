5783149c623b165a87d941fb85ae78c48ead5f47
Kris Urbanski <kris@whereibend.space>
2022-10-09 16:34:25 +0200

    autosave

--- src/lib.rs
+++ src/lib.rs
@@ -12,4 +12,28 @@ Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
+
+    #[test]
+    fn parse_this() {
+    let args: Vec<String> = vec![String::from("0"), String::from("a"), String::from("b")];
+
+    let config = Config::new(&args);
+    assert_eq!(config.query, "a");
+    assert_eq!(config.file_path, "b");
+    }
+
+    #[test]
+    fn build_this() {
+    let args: Vec<String> = vec![String::from("0"), String::from("a"), String::from("b")];
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    assert_eq!(config.query, "a");
+    assert_eq!(config.file_path, "b");
+    }
+
 }
