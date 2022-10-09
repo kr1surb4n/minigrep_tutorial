1c0bb1c41964444e3f8c9fda388476ca8989365b
Kris Urbanski <kris@whereibend.space>
2022-10-09 17:18:42 +0200

    autosave

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     // ]
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
