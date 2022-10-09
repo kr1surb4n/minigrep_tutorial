f757146ea1a2daa79f8707dbee628bf8c3dc3a91
Kris Urbanski <kris@whereibend.space>
2022-10-09 14:17:18 +0200

    autosave

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,9 @@ fn main() {
     // get all command line args and load them into structure
     let args: Vec<String> = env::args().collect();
     dbg!(args);
+    // args = [
+    // "target/debug/minigrep",
+    // "searchstring",
+    // "example-filename.txt",
+    // ]
 }
