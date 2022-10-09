fbd2ca34fb6f0312156893a6ccb5bcbb7555b3be
Kris Urbanski <kris@whereibend.space>
2022-10-09 15:31:20 +0200

    autosave

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,16 @@ fn main() {
     println!("With text:\n{contents}");
 }
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
 fn parse_config(args: &[String]) -> (&str, &str) {
     let query = &args[1];
     let file_path = &args[2];
 
-    (query, file_path)
+    Config { query, file_path }
 }
 
 #[test]
