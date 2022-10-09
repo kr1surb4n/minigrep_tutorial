6fb67234d418f6a3dcb57cd3e56779c6871a1746
Kris Urbanski <kris@whereibend.space>
2022-10-09 16:34:13 +0200

    autosave

--- src/main.rs
+++ src/main.rs
@@ -60,25 +60,3 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
-
-#[test]
-fn parse_this() {
-    let args: Vec<String> = vec![String::from("0"), String::from("a"), String::from("b")];
-
-    let config = Config::new(&args);
-    assert_eq!(config.query, "a");
-    assert_eq!(config.file_path, "b");
-}
-
-#[test]
-fn build_this() {
-    let args: Vec<String> = vec![String::from("0"), String::from("a"), String::from("b")];
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
-
-    assert_eq!(config.query, "a");
-    assert_eq!(config.file_path, "b");
-}
