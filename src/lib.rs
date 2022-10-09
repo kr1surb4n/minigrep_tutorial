5f8ad20a692ed0fbe8afac1710e827d87790568f
Kris Urbanski <kris@whereibend.space>
2022-10-09 16:44:41 +0200

    autosave

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,25 @@
 use std::process;
 
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
 
