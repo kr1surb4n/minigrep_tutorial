08be907e9f7fdea274b40ed19310c5d020aabcb3
Kris Urbanski <kris@whereibend.space>
2022-10-09 15:39:34 +0200

    autosave

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,17 @@ impl Config {
 
         Config { query, file_path }
     }
+
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
 
 #[test]
