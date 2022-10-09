16ebd920b5caf1ee09f3c2664cdffa70b6f40cc5
Kris Urbanski <kris@whereibend.space>
2022-10-09 14:18:04 +0200

    autosave

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,16 @@ use std::env;
 fn main() {
     // get all command line args and load them into structure
     let args: Vec<String> = env::args().collect();
+    // will panic on non-unicode sign
+    // std::env::args_os -> unicode safe
+
+
     dbg!(args);
     // args = [
     // "target/debug/minigrep",
     // "searchstring",
     // "example-filename.txt",
     // ]
+
+
 }
