1a3928fb3dcf3c86e0ac79799a429b896ff3c4db
Kris Urbanski <przemek.kris.urbanski@gmail.com>
2023-03-08 15:46:54 +0100

    Last commit

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,17 @@ use std::env;
 use std::process;
 
 fn main() {
-    // get all command line args and load them into structure
-    let args: Vec<String> = env::args().collect();
-    // will panic on non-unicode sign
-    // std::env::args_os -> unicode safe
-
-    dbg!(&args);
+    // dbg!(&args);
     // args = [
     // "target/debug/minigrep",
     // "searchstring",
     // "example-filename.txt",
     // ]
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    // get all command line args (env::args())
+    // will panic on non-unicode sign
+    // std::env::args_os -> unicode safe
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
